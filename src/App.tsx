ee2bbc78a094feeb102552d4804429261e472286
namlvh <lvhn1601@gmail.com>
2024-04-19 20:17:02 +0700

    init + create layout + signup page

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router-dom'
+
+import './globals.css'
+
+
+import SignInForm from './_auth/forms/SignInForm'
+import SignUpForm from './_auth/forms/SignUpForm'
+import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/signin' element={<SignInForm />}/>
+          <Route path='/signup' element={<SignUpForm />}/>
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
